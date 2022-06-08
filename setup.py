6fa81299e4311c88aee640bbede7d73f298ccc3e
nikwl <nikolas.lamb@gmail.com>
2022-06-08 14:27:05 -0400

    added setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name="meshflip",
+    version="0.01",
+    packages=["meshflip",],
+    license="MIT",
+    long_description=open("README.txt").read(),
+)
